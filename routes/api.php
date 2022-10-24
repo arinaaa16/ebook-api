b964f4266d0c202a79bcceca8fb0e62d80712c06
arinaaa16 <arinaintanrizzz16@gmail.com>
2022-10-24 14:05:26 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('me',function(){
+    return ["arina" => "cuantik"];
 });
+
+Route::resource('halocontroller', HeloController::class);
+Route::resource('siswa', SiswaController::class);
